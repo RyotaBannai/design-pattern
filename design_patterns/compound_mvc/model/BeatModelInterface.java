8c51d1ae244f82f8c27473d01615cfe2a4d1b16e
RyotaBannai <ryotala0528@gmail.com>
2020-09-20 20:22:18 +0900

    add: mvc pattern

---
+++ design_patterns/compound_mvc/model/BeatModelInterface.java
@@ -0,0 +1,21 @@
+package design_patterns.compound_mvc.model;
+
+public interface BeatModelInterface {
+  void initialize();
+
+  void on();
+
+  void off();
+
+  void setBPM(int bpm);
+
+  int getBPM();
+
+  void registerObserver(BeatObserver o);
+
+  void removeObserver(BeatObserver o);
+
+  void registerObserver(BPMObserver o);
+
+  void removeObserver(BPMObserver o);
+}
